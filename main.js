4eac2b179a916d328da47eb6be4baf6764119410
palsparr <patrik.alsparr@gmail.com>
2016-01-08 06:30:01 +0100

    awd

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ var server = http.createServer(function(request, response) {
             if (parsedRequest.query.topicID) {
                 dbClient.connect();
                 var query = dbClient.query('CREATE TABLE IF NOT EXISTS topics (id TEXT, followers INTEGER)');
-                query = dbClient.query('INSERT INTO topics (id, followers) VALUES (' + parsedRequest.query.topicID + ', ' + 0 + ')');
+                query = dbClient.query('INSERT INTO topics (id, followers) VALUES (' + "'" + parsedRequest.query.topicID + "'" + ', ' + 0 + ')');
                 query = dbClient.query('CREATE TABLE IF NOT EXISTS ' + parsedRequest.query.topicID + ' (messages TEXT)');
                 query.on('end', function() { dbClient.end(); });
                 response.writeHead(200, {"Content-Type": "text"});
