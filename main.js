9eaf011a9ef77b435dc22c97cde8de010bddbd7e
palsparr <patrik.alsparr@gmail.com>
2016-01-09 15:40:21 +0100

    awd

--- main.js
+++ main.js
@@ -16,40 +16,21 @@ var server = http.createServer(function(request, response) {
     
     switch(parsedRequest.pathname) {
         case "/searchTopics":
-            
+            var searchKey = parsedRequest.query.key;
+            if (searchKey) {
+                searchTopics(searchKey);   
+            }
             break;
         case "/createTopic":
-            if (parsedRequest.query.topicID) {
-                var query = dbClient.query('CREATE TABLE IF NOT EXISTS topics (id TEXT, followers INTEGER)');
-                query = dbClient.query('INSERT INTO topics (id, followers) VALUES (' + "'" + parsedRequest.query.topicID + "'" + ', ' + 0 + ')');
-                query.on('row', function(row) {
-                    console.log(row.id + ' ' + row.followers); 
-                });
-                query = dbClient.query('CREATE TABLE IF NOT EXISTS ' + parsedRequest.query.topicID + ' (messages TEXT)');
-                
-                response.writeHead(200, {"Content-Type": "text"});
-                response.write("hej");
-                response.end();
+            var topicID = parsedRequest.query.topicID;
+            if (topicID) {
+                createTopic(topicID);
             }
             break;
         case "/followTopic":
             
-            var followers = 0;
             var topicID = parsedRequest.query.topicID;
-            var query = dbClient.query('SELECT followers FROM topics WHERE id = ' + "'" + topicID + "'");
-            console.log('SELECT followers FROM topics WHERE id = ' + "'" + topicID + "'");
-            query.on('row', function(row) {
-                followers = row.followers + 1;
-                console.log(followers);
-                query = dbClient.query('UPDATE topics SET followers = ' + followers + ' WHERE id = ' + "'" + topicID + "'");
-            });
-            
-            responseJSONObject.messages = messageArray;
-            response.writeHead(200, {"Content-Type": "text"});
-            responseJSONObject.code = 200;
-            responseJSONObject.request = parsedRequest.pathname;
-            response.write(JSON.stringify(responseJSONObject));
-            response.end();
+            followTopic(topicID);
             
             break;
         case "/sendMSG":
@@ -57,27 +38,16 @@ var server = http.createServer(function(request, response) {
             var topicID = parsedRequest.query.topicID;
             if (message) {
                 if (topicID) {
-                    var query = dbClient.query('INSERT INTO ' + topicID  + ' (messages) VALUES (' + "'" + message + "'" + ')');
-                    console.log(topicID);
+                    sendMessage(message, topicID);
                 }
             }
-            query = dbClient.query('SELECT * FROM ' + topicID);
-            query.on('row', function(row) {
-                messageArray.push(row.messages); 
-                console.log('kommer hit');
-                console.log(row.messages);
-                responseJSONObject.messages = messageArray;
-                response.writeHead(200, {"Content-Type": "text"});
-                responseJSONObject.code = 200;
-                responseJSONObject.request = parsedRequest.pathname;
-                response.write(JSON.stringify(responseJSONObject));
-                response.end();
-            });
+            getPreviousMsgs(topicID);
+            
             //sendMessage(message, 1);
             
             break;
         case "/getPreviousMsgs":
-            
+            getPreviousMsgs(parsedRequest.query.topicID);
             break;
         case "/getFollowedTopics":
             
@@ -97,34 +67,80 @@ var server = http.createServer(function(request, response) {
                     
             
     };
-    
+                    
+    function searchTopics(keyword) {
+        var query = dbClient.query('SELECT * FROM topics WHERE to_tsvector(id) @@ plainto_tsquery(' + "'" + keyword + "')");
+                query.on('row', function(row) {
+                    var topic = row;
+                    responseArray.push(topic);
+                    
+                });
+                query.on('end', function() {
+                    responseJSONObject.searchResults.topics = responseArray;
+                    response.writeHead(200, {"Content-Type": "text"});
+                    responseJSONObject.code = 200;
+                    responseJSONObject.request = parsedRequest.pathname;
+                    response.write(JSON.stringify(responseJSONObject));
+                    response.end();                    
+                });
+        
+    }
+    function createTopic(topicID) {
+        var query = dbClient.query('CREATE TABLE IF NOT EXISTS topics (id TEXT, followers INTEGER)');
+                query = dbClient.query('INSERT INTO topics (id, followers) VALUES (' + "'" + topicID + "'" + ', ' + 1 + ')');
+                query.on('row', function(row) {
+                    console.log(row.id + ' ' + row.followers); 
+                });
+                query = dbClient.query('CREATE TABLE IF NOT EXISTS ' + topicID + ' (messages TEXT)');
+                
+                response.writeHead(200, {"Content-Type": "text"});
+                response.write("hej");
+                response.end();
+
+    }
+    function followTopic(topicID) {
+        var followers = 0;
+        var query = dbClient.query('SELECT followers FROM topics WHERE id = ' + "'" + topicID + "'");
+            console.log('SELECT followers FROM topics WHERE id = ' + "'" + topicID + "'");
+            query.on('row', function(row) {
+                followers = row.followers + 1;
+                console.log(followers);
+                query = dbClient.query('UPDATE topics SET followers = ' + followers + ' WHERE id = ' + "'" + topicID + "'");
+            });
+            getPreviousMsgs(topicID);
+    }
+    function sendMessage(message, topicID) {
+        var query = dbClient.query('INSERT INTO ' + topicID  + ' (messages) VALUES (' + "'" + message + "'" + ')');
+        console.log(topicID);
+    }
+    function getPreviousMsgs(topicID) {
+        query = dbClient.query('SELECT * FROM ' + topicID);
+                query.on('row', function(row) {
+                    messageArray.push(row.messages); 
+                    console.log('kommer hit');
+                    console.log(row.messages);
+                });
+        query.on('end', function() { 
+                    responseJSONObject.messages = messageArray;
+                    response.writeHead(200, {"Content-Type": "text"});
+                    responseJSONObject.code = 200;
+                    responseJSONObject.request = parsedRequest.pathname;
+                    response.write(JSON.stringify(responseJSONObject));
+                    response.end();
+        });
+    }
+    function getFollowedTopics(user) {
+
+    }
+    function getTopTopics() {
+
+    }
+    function update(topicID) {
+        responseJSONObject.messages = messageArray;
+    }   
     
 });
 
 server.listen(80);
 console.log("Listening... Ya bitch!");
 
-function searchTopics(keyword) {
-    
-}
-function createTopic(topicID) {
-    
-}
-function followTopic(topicID) {
-
-}
-function sendMessage(message, topicID) {
-    messageArray.push(message);
-}
-function getPreviousMsgs(topicID) {
-    
-}
-function getFollowedTopics(user) {
-    
-}
-function getTopTopics() {
-    
-}
-function update(topicID) {
-    responseJSONObject.messages = messageArray;
-}
